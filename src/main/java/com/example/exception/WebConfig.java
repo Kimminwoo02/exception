e3dcc00e1c0d475aed8121e8a341450706273742
minu <made_power@naver.com>
2023-04-28 20:31:24 +0900

    서블릿 예외 처리 - 인터셉터
    인터셉터를 이용한 예외처리 방법

--- src/main/java/com/example/exception/WebConfig.java
+++ src/main/java/com/example/exception/WebConfig.java
@@ -1,15 +1,25 @@
 package com.example.exception;
 
 import com.example.exception.filter.LogFilter;
+import com.example.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**","*/ico","/error","/error-page/**"); // 오류 페이지 경로
+    }
+
+    //@Bean
     public FilterRegistrationBean logFilter(){
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -18,4 +28,6 @@ public class WebConfig implements WebMvcConfigurer {
         filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST,DispatcherType.ERROR);
         return filterRegistrationBean;
     }
+
+
 }
